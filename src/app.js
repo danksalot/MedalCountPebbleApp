fe860cdc8a153810f6ddddbfa20addfc733c3e01
danksalot <danksalot@users.noreply.github.com>
2016-08-03 00:36:56 -0500

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,35 @@
+var UI = require('ui');
+var ajax = require('ajax');
+
+var splashScreen = new UI.Card({
+  title: "Medal Count",
+  body: "Loading...",
+  banner: 'images/Rings_135.png'
+});
+splashScreen.show();
+
+ajax({ url: 'https://app.scrapinghub.com/api/jobs/list.json?project=84590&apikey=f3c8dc917c7444e7aa903a582c42f8f6&count=1', type: 'json' },
+  function(jobsList) {
+    ajax({ url: 'https://storage.scrapinghub.com/items/' + jobsList.jobs[0].id + '?apikey=f3c8dc917c7444e7aa903a582c42f8f6&format=json', type: 'json' },
+      function(data) {
+        var items = [];
+        
+        data.forEach(function(obj) { 
+          items.push({
+            title: obj.place + " " + obj.country, 
+            subtitle: "G:" + obj.gold + " S:" + obj.silver + " B:" + obj.bronze + " Tot:" + obj.total
+          }); 
+        });
+        
+        var menu = new UI.Menu({
+          sections:[{
+            items:items
+          }]
+        });    
+        
+        splashScreen.hide();
+        menu.show();
+      }
+    );
+  }
+);
